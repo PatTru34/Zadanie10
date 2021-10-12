846dc4af9c2408502114825a1fde4366b46cfb1b
Patryk Truchan <162391@student.uwm.edu.pl>
2021-10-12 10:05:34 +0200

    Initial commit

---
+++ src/pl/edu/uwm/wmii/kotewa/laboratorium00/Zadanie10.java
@@ -0,0 +1,10 @@
+package pl.edu.uwm.wmii.kotewa.laboratorium00;
+
+public class Zadanie10 {
+
+    public static void main(String[] args) {
+        System .out. println ("Abraham Lincoln Pogromca Wampirów");
+        System .out. println ("Sharknado");
+        System .out. println ("Saga Zmierzch");
+    }
+}
